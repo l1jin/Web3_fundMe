b114f399b31c4b8af4e73603c3502cce3b58cae8
lijin <382411345@qq.com>
2025-07-05 14:29:10 +0800

    fundme测试和部署脚本

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,9 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@chainlink/env-enc").config();
 require("./tasks");
+require("hardhat-deploy")
+require("hardhat-deploy-ethers");
+require("@nomicfoundation/hardhat-ethers");
 const SEPOLIA_URL = process.env.SEPOLIA_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const PRIVATE_KEY_1 = process.env.PRIVATE_KEY_1
@@ -8,6 +11,10 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  defaultNetwork: "hardhat",
+  mocha: {
+    timeout: 300000
+  },
   networks: {
     // hardhat: {
     //   chainId: 1337,
@@ -22,5 +29,16 @@ module.exports = {
     apiKey: {
       sepolia: ETHERSCAN_API_KEY,
     }
+  },
+  namedAccounts: {
+    firstAccount: {
+      default: 0
+    },
+    secondAccount: {
+      default: 1
+    },
+  },
+  gasReporter: {
+    enabled: true
   }
 };
